130d1f26e166c4d2f1a32ae0e3fb0a8be9d77b87
cambo2015 <cameronhansen91@outlook.com>
2019-01-25 20:49:46 -0700

    added Bubble Sort in python
    This my not be the best Bubble Sort Algorithm implementation, but it gets the job done

---
+++ bubbleSort.py
@@ -0,0 +1,25 @@
+def bubbleSort(l):#->void
+    sorted = False
+    while not sorted:
+        sorted = True
+        for i in range(len(l)-1):
+            a = l[i]
+            b = l[i+1]
+            if l[i] > l[i+1] :
+                temp = l[i]
+                l[i] = l[i+1]
+                l[i+1] = temp
+                sorted = False
+    return l
+
+def showAll(l):
+    for i in range(len(l)):
+        print(l[i])
+
+
+def main():
+    l = [1,5,7,4,7,4]
+    l = bubbleSort(l)
+    showAll(l)
+    
+main()
