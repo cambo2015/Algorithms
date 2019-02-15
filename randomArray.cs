46c0cfb509c474c33bb3853c735dc40130d6f38c
cambo2015 <cameronhansen91@outlook.com>
2019-02-15 06:41:17 -0700

    random array added

---
+++ randomArray.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using static Dcoder.Algorithms;
+using static System.Console;
+namespace Dcoder
+{
+	
+	public static class Program
+	{
+		public static void Main(string[] args)
+		{
+			List<int> l = CreateArray(10);
+			foreach(var n in l)
+				Write(n);
+		}
+	}
+	
+	public static class Algorithms
+	{
+		public static List<int> CreateArray(int max = 10)
+		{
+			Random r = new Random();
+			var l = new List<int>();
+			for(int i =0;i<max;i++)
+			{
+				while(true)
+				{
+					var n = r.Next(0,10);
+					if(l.Contains(n))
+					{
+						continue;
+					}
+					l.Add(n);
+					break;
+				}
+			}
+			return l;
+		}
+	}
+}
